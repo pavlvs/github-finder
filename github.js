962cb4e02d9eb19f7c41e874fc640b04c98b0718
pavlvs <pavlvsx@gmail.com>
2018-01-27 00:56:45 +0100

    add fetching repos

--- github.js
+++ github.js
@@ -2,16 +2,21 @@ class GitHub {
     constructor() {
         this.client_id = 'bb968a4dacdcf09ff4ef';
         this.client_secret = '95ab9f84f4af83d4a33a42a64b6b0fe739bfadbf';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-        const profileData = await profileResponse.json();
-        console.log(profileData);
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
 
         return{
-            profile: profileData
+            profile,
+            repos
         }
         
     }
--- ui.js
+++ ui.js
@@ -9,7 +9,7 @@ class UI {
             <div class="row">
                 <div class="col-md-3">
                     <img src="${user.avatar_url}" alt="" class="img-fluid">
-                    <a href="${user.html_url}" class="btn btn-primary btn-block" target="_blank">View Profile</a>
+                    <a href="${user.html_url}" class="btn btn-primary btn-block mt-2" target="_blank">View Profile</a>
                 </div>
                 <div class="col-md-9">
                     <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
@@ -30,6 +30,28 @@ class UI {
           <h3 class="page-heading mb-3">Latest Repos</h3>
           <div id="repos"></div>
         `;
+    }
+
+    showRepos(repos) {
+        let output = '';
+
+        repos.forEach(repo => {
+            output += `
+                <div class="card card-body mb-2">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="${repo.html_url}">${repo.name}</a>
+                        </div>
+                        <div class="col-md-6">
+                        <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                        <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+                        <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+                        </div>
+                    </div>
+                </div>
+            `
+        });
+        document.getElementById('repos').innerHTML = output;
     }
 
     clearProfile() {
